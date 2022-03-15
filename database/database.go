9aa3695d04e0e3499e2ae1d4f188e45a305553c5
caiostarke <caiostarke@hotmail.com>
2022-03-15 19:31:12 -0300

    Added environment variables

--- database/database.go
+++ database/database.go
@@ -1,9 +1,12 @@
 package database
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/caiostarke/studyToAdvanced/models"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -14,7 +17,18 @@ var (
 )
 
 func Connect() {
-	DB, err = gorm.Open(mysql.Open("root:eduardo3@tcp(localhost:3306)/blogorm"))
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	MYSQL_ROOT_PASSWORD := os.Getenv("MYSQL_ROOT_PASSWORD")
+	DB_USER := os.Getenv("DB_USER")
+	DB_HOST := os.Getenv("DB_HOST")
+	DB_NAME := os.Getenv("DB_NAME")
+
+	URL := fmt.Sprintf("%s:%s@tcp(%s)/%s", DB_USER, MYSQL_ROOT_PASSWORD, DB_HOST, DB_NAME)
+
+	DB, err = gorm.Open(mysql.Open(URL))
 
 	if err != nil {
 		log.Fatal("Failed connect to DB")
